c549a0aa4867d3505273d36358a579e9f7c5f563
Maciej Hedloff <mhedloff@gmail.com>
2021-05-10 08:13:52 +0200

    MH: dockerized

--- authServer/src/database/migrations/1617733023847-InsertRoles.ts
+++ authServer/src/database/migrations/1617733023847-InsertRoles.ts
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 import { Role } from "../../dataModel/Role";
 import { UserRole } from "../entities/UserRole";
 import {RolePermissionMapping} from "../../dataModel/RolePermissionMapping";
@@ -7,21 +7,49 @@ import {Permission} from "../../dataModel/Permission";
 
 export class InsertRoles1617733023847 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
-        const entities = Role.values.map((userRole: Role) => {
-            return new UserRole(
-              userRole.id,
-              userRole.name,
-              RolePermissionMapping.getByRole(userRole)
-            );
-        });
-        const permissions = Permission.values.map((permission) => {
-            return new UserPermission(permission.id, permission.name);
-        });
-        await queryRunner.manager.save<UserPermission>(permissions);
-        await queryRunner.manager.save<UserRole>(entities);
+      await this.createTables(queryRunner);
+
+      const permissions = Permission.values.map((permission) => {
+          return new UserPermission(permission.id, permission.name);
+      });
+      const entities = Role.values.map((userRole: Role) => {
+          return new UserRole(
+            userRole.id,
+            userRole.name,
+            RolePermissionMapping.getByRole(userRole)
+          );
+      });
+      await queryRunner.manager.save<UserPermission>(permissions);
+      await queryRunner.manager.save<UserRole>(entities);
     }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
+  private async createTables(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: "user_permission",
+      columns: [
+        {name: "id", type: "integer", isGenerated: true, isPrimary: true},
+        {name: "name", type: "varchar"}
+      ]
+    }));
+
+    await queryRunner.createTable(new Table({
+      name: "user_role",
+      columns: [
+        {name: "id", type: "integer", isPrimary: true, isGenerated: true},
+        {name: "name", type: "varchar"}
+      ]
+    }));
+
+    await queryRunner.createTable(new Table({
+      name: "user_role_permissions_user_permission",
+      columns: [
+        {name: "userRoleId", type: "integer"},
+        {name: "userPermissionId", type: "integer"}
+      ]
+    }));
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
         const userRoleDeletes = Role.values
           .map((role: Role) => queryRunner.manager.delete(UserRole, role.id));
         const permissionDeletes = Permission.values
